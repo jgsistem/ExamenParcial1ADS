778567c7b380b2c6df92ae2da7d277bb9b1c9f27
jgsistem <jgsistem@hotmail.com>
2018-06-02 13:59:41 -0500

    Subir

--- Combinations.java
+++ Combinations.java
@@ -26,7 +26,7 @@ public class Combinations {
 		long respt2= countCombinationsN3(n - 2,level + 1);
 		long respt3= countCombinationsN4(n - 3,level + 1);
 		
-		return  respt1 + respt2 + respt3;
+		return result + respt1 + respt2 + respt3;
 	}
 	
 	public long countCombinationsN2(int n, int level) {					
